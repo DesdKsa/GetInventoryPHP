099ee0040607b8a1dc8fcfcee8497e428a2d6fa7
DesdKsa Market <148357192+DesdKsa@users.noreply.github.com>
2023-10-18 12:36:15 -0200

    Corrección y optimización.
    Se realizaron varias correcciones y ciertos cambios en el código para ofrecer una experiencia más sencilla. Y más optimizado

--- api.php
+++ api.php
@@ -1,41 +1,35 @@
 <?php
-// URL base de la API
-$api_url = "https://api.desdksamarket.com/v1/inventory/";
+/*Get inventory*/
+define("API_URL", "https://api.desdksamarket.com/v1/inventory/");
+define("API_KEY", "your-api-key");
 
-// Clave de la API
-$api_key = "7uBtYp2rKsQ6vDg9F3xM1wP5oZ8nA0eL";
+function fetchAndSaveData() {
+    $url = API_URL . "?key=" . API_KEY;
 
-// Construimos la URL completa con la clave como parámetro
-$url = $api_url . "?key=" . $api_key;
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($ch);
 
-// Inicializamos una sesión cURL
-$ch = curl_init($url);
+    if (curl_getinfo($ch, CURLINFO_HTTP_CODE) == 200) {
+        $data = json_decode($response, true);
 
-// Configuramos las opciones de cURL
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $output = [];
+        foreach ($data as $product) {
+            $output[] = [
+                'Name' => utf8_encode($product['Product_title']),
+                'Description' => utf8_encode($product['Product_info']),
+                'Price' => utf8_encode($product['Product_price'])
+            ];
+        }
 
-// Realizamos la solicitud GET a la API
-$response = curl_exec($ch);
-
-// Verificamos si la solicitud fue exitosa
-if (curl_getinfo($ch, CURLINFO_HTTP_CODE) == 200) {
-    // Decodificamos los datos de respuesta
-    $data = json_decode($response, true);
-
-    // Guardamos los datos en un archivo de texto con codificación UTF-8
-    $file = fopen("products.txt", "w");
-    foreach ($data as $product) {
-        fwrite($file, "Name: " . utf8_encode($product['Product_title']) . "\n");
-        fwrite($file, "Description: " . utf8_encode($product['Product_info']) . "\n");
-        fwrite($file, "Price: " . utf8_encode($product['Product_price']) . "\n\n");
+        file_put_contents("products.json", json_encode($output, JSON_PRETTY_PRINT));
+        echo "The products have been saved in products.json";
+    } else {
+        echo "Error in the request: Code " . curl_getinfo($ch, CURLINFO_HTTP_CODE);
     }
-    fclose($file);
 
-    echo "The products have been saved in products.txt";
-} else {
-    echo "Error in the request: Code " . curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
 }
 
-// Cerramos la sesión cURL
-curl_close($ch);
+fetchAndSaveData();
 ?>
